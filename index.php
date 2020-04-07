6b72bd49037c10f961cdeddb5b999631993a9ba4
rahul499 <rahul4mishra99@gmail.com>
2020-04-07 22:06:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Spice Food</title>
+      <link rel="stylesheet" href="css/style.css">
+<style>
+  body{
+    background-color:snow;
+  }
+</style>
+
+</head>
+<body>
+
+
+
+<div class="list">
+<ul>
+	
+	<li class="title"><a href="index.php">SPICE FOOD</a>
+  </li>  <img src="images/logo.png" alt="logo" style="width:100px; height:80px; border:2px solid black;">
+   
+   <li><a href="login.php">ADMIN</a></li>
+	 <li><a href="contact.php">CONTACT US</a></li>
+	 <li><a href="menu.php">MENU</a></li>
+	  <li><a href="about.php">ABOUT US</a></li>
+	  <li><a href="index.php">HOME</a></li>      
+</ul>
+</div>
+
+
+ <!--================Home Banner Area =================-->
+        <section class="home_slider">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+						<div class="slider_text">
+							<img src="images/slider-1.jpg" alt="">
+						</div>				
+				</div>
+			</div>
+  
+        </section>
+     
+<!--================End Home Banner Area =================-->
+  
+   <center> <div  id="qwe" style="background-color: snow;">
+      <h1 style="color:black; "><em>Spice food</em></h1><br> </div>
+      <div ><img src="images/h1.jpeg" class="slide" border=4 height=450px width=600px>
+      <div class="abc" style="background-color:#F0F8FF;black:white;padding:20px;">
+      <p>From your neighborhood sushi spot to the burger and fries you crave, choose from over 300 Global favorites dishes
+    across the whole Globe. 
+    Spice Food is one of the most famous restaurants in Hamilton. Known well for it’s freshly prepared and delicious food, We offer an ideal place to spend quality time while enjoying the food. The place is one of the best restaurants in Hamilton and has a vibrant theme that attracts young people easily.  </p> </div>
+</div></center>
+
+
+    <div class="container">
+      <br><br><br><br>
+           
+    <center>  
+     <h2>Our <em>Speciality</em></h2>
+     <br>
+    	<main>
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/h2.jpg" alt="" style="width:270px; height:270px;">
+          <div class="blogContent">
+            <h2>Fruit Cake </h2>    
+            <a href="#" class="btn">$60.00</a>
+          </div>
+        </div>
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/h4.jpeg" alt="" style="width:270px; height:270px;">
+          <div class="blogContent">
+            <h2>Noodles with Vegetables </h2>
+            <a href="#" class="btn">$8.65</a>
+          </div>
+        </div>
+        
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/home6.jpg" alt="" style="width:270px; height:270px;">
+          <div class="blogContent">
+            <h2>Chicken Biryani</h2>
+            <a href="#" class="btn">$20.50</a>
+          </div>
+        </div>
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/home7.jpeg" alt="" style="width:270px; height:270px;">
+          <div class="blogContent">
+            <h2>Italian Pasta</h2>
+            <a href="#" class="btn">$30.75</a>
+          </div>
+        </div>
+         <br><br>
+      </main>
+    </center>
+    </div>
+    <br>
+  
+    <center>  
+     <h2> <em>Special Offers on these dishes</em></h2>
+     <br>
+      <main>
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/h3.jpeg" alt="" style="width:350px; height:270px;">
+          <div class="blogContent">
+            <h2>Chocolate Cake</h2>
+            <a href="#" class="btn">$5.00</a>
+          </div>
+        </div>
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/h5.jpeg" alt="" style="width:350px; height:270px;">
+          <div class="blogContent">
+            <h2>Baked Pizza</h2>
+            <a href="#" class="btn">$25.00</a>
+          </div>
+        </div>
+        
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/h6.jpeg" alt="" style="width:350px; height:270px;">
+          <div class="blogContent">
+            <h2>French Fries with Dipped sauce</h2>
+            <a href="#" class="btn">$2.00</a>
+          </div>
+        </div>
+        <div class="singleBlog" style="border:2px solid black">
+          <img src="images/h7.jpeg" alt="" style="width:350px; height:270px;">
+          <div class="blogContent">
+            <h2>Grilled Kebab</h2>
+            <a href="#" class="btn">$10.50</a>
+          </div>
+        </div>
+      </main>
+    </center>
+    </div>
+    <br><br><br><br>
+
+  
+	<div class="abd">
+		<marquee behavior="scroll" direction="right" ><img src="images/menu6.jpg" style="width:250px;height:200px"><img src="images/menu7.jpg"style="width:250px;height:200px"><img src="images/menu8.jpg" style="width:250px;height:200px"><img src="images/menu9.jpg" style="width:250px;height:200px"><img src="images/menu10.jpg" style="width:250px;height:200px"><img src="images/menu11.jpg" style="width:250px;height:200px"><img src="images/menu12.jpg" style="width:250px;height:200px"><img src="images/menu13.jpg" style="width:250px;height:200px"><img src="images/menu14.jpg" style="width:250px;height:200px"><img src="images/menu15.jpg" style="width:250px;height:200px">
+	</marquee></div>
+
+<div class="abc" style="background-color:pink;black:white;padding:20px;">
+Contact Us Here <br>
+Spice Food Restaurant<br>
+Address: TE RAPA ROAD BASE<br>
+Timmings: 11:00am - 12:00pm Everyday<br>
+Phone: 0226035534<br>
+Email: khoslaabhi3@gmail.com<br>
+</div>
+
+
+
+
+
+</body>
+</html>
