2272ed66c298710d22341243088776dd75bea3ea
rahul499 <rahul4mishra99@gmail.com>
2020-04-07 22:07:56 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Spice Food</title>
+      <link rel="stylesheet" href="css/about.css">
+       <link rel="stylesheet" href="css/style.css">
+
+<style>
+  .w3-xxlarge{
+    color:white;
+  }
+
+  .w3-display-container w3-content w3-wide{
+    border:4px 
+  }
+  body{
+    background-color: #B0E0E6;
+  }
+
+</style>
+</head>
+<body>
+
+
+
+<div class="list">
+<ul>
+  
+  <li class="title"><a href="index.php">SPICE FOOD</a>
+  </li>  <img src="images/logo.png" alt="logo" style="width:100px; height:80px; border:2px solid black;">
+   
+   <li><a href="login.php">ADMIN</a></li>
+   <li><a href="contact.php">CONTACT US</a></li>
+   <li><a href="menu.php">MENU</a></li>
+    <li><a href="about.php">ABOUT US</a></li>
+    <li><a href="index.php">HOME</a></li>      
+</ul>
+</div>
+
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="height:450px; width:100%" id="home">
+  <img class="w3-image" src="images/menuu.jpeg" alt="Hamburger Catering" style="width:1600px; height:450px;">
+  <div class="w3-display-bottomleft w3-padding-large w3-opacity">
+    <h1 class="w3-xxlarge" >SPICE FOOD</h1>
+  </div>
+</header>
+<br><br><br>
+
+
+
+
+<center>
+<div class="chefcard">
+   <h1 class="w3-center" style="color:black;">Our Menu</h1><br><br>
+<div class="card">
+  <img src="images/menu1.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Shesh Kebab</b></h4>
+    <p>$24.50</p>
+  </div>
+</div>
+
+
+<div class="card">
+  <img src="images/menu2.jpeg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Baked Chicken</b></h4>
+    <p>$20.00</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu3.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Achari Paneer</b></h4>
+    <p>$15.00</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu4.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Poke </b></h4>
+    <p>$30.50</p>
+  </div>
+</div>
+
+
+
+<br><br><br>
+
+<div class="card">
+  <img src="images/menu5.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Aguachido</b></h4>
+    <p>$20.00</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu6.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Old Forge Pizza </b></h4>
+    <p>$10.75</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu7.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Reuben Sandwich</b></h4>
+    <p>$4.50</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu8.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Jalebi </b></h4>
+    <p>$12.60</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu9.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Meze</b></h4>
+    <p>$15.75r</p>
+  </div>
+</div>
+
+<br><br><br>
+
+<div class="card">
+  <img src="images/menu10.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Potato Wedges</b></h4>
+    <p>$6.50</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu11.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Fugu “Puffer Fish”</b></h4>
+    <p>$20.00</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu12.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Truffle Meal</b></h4>
+    <p>$12.50</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu13.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Samoosas</b></h4>
+    <p>$5.00</p>
+  </div>
+</div>
+
+
+<br><br><br>
+
+<div class="card">
+  <img src="images/menu14.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Troll King Salmon</b></h4>
+    <p>$45.00</p>
+  </div>
+</div>
+
+
+
+<div class="card">
+  <img src="images/menu15.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Poutine</b></h4>
+    <p>$60.50</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu16.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Bannock</b></h4>
+    <p>$75.00</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="images/menu17.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Lobster Rolls</b></h4>
+    <p>$45.50</p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/menu18.jpg" alt="Avatar" style="height:150px; width:170px;">
+  <div class="container">
+    <h4><b>Berry pie</b></h4>
+    <p>$20.75</p>
+  </div>
+</div>
+
+</div>
+</center><br><br><br>
+
+
+<div class="w3-content" >
+  <h1 class="w3-center" style="color:black;"><b>Our Special Dishes</b></h1><br>
+
+  <div class="w3-row w3-padding-64" id="about">
+  <div class="w3-col m6 w3-padding-large w3-hide-small">
+  <img src="images/h2.jpg" class="w3-round w3-image w3-opacity-min" alt="Table Setting" style="width:800px; 
+  height:590px;">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+     <h1 class="w3-center" style="color:black;">Fruit Cake</h1><br>
+      <p class="w3-large"> Fruit Cake is made with candied or dried fruit, nuts, and spices, and optionally soaked in spirits. Fruitcake is perfect for the holidays. It's light, tender, and full of dried fruits and nuts. Sprinkle the loaf with brandy, or leave it out! Either way, this fruit cake is a welcome addition to your table. In this Season strawberries and raspberries are used but you could also use blueberries, blackberries or other seasonal berries</p>
+      <p class="w3-large w3-text-grey w3-hide-medium">A wonderfully fluffy sponge cake filled with creamy icing and topped with delicious fruit, our Fruits Cake is an instant classic, a delectable bake with a true taste of Summer, try it today!</p>
+    </div>
+  </div>
+  
+  <hr >
+  
+  <div class="w3-row w3-padding-64" id="about">
+  <div class="w3-col m6 w3-padding-large w3-hide-small">
+  <img src="images/h4.jpeg" class="w3-round w3-image w3-opacity-min" alt="Table Setting" style="width:800px; 
+  height:590px;">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+     <h1 class="w3-center" style="color:black;">Noodles With Vegetables</h1><br>
+      <p class="w3-large"> Nothing ticks our comfort food box like an easy Asian vegetable stir fry with noodles.  Tender vegetables and al-dente noodles covered with just the right amount of thick tasty tangy sauce… We all get hungry just thinking about it. A super-satisfying and vegan easy weeknight dinner dish. The best thing about this easy Asian vegetable stir fry is that you can add practically any combo of vegetables to it. </p>
+      <p class="w3-large w3-text-grey w3-hide-medium">Broccoli, water chestnuts, sliced chilis, and baby corn would also be awesome in this, with some tofu and/or edamame thrown in…you do you sugar and use whichever vegetables you wish. It’s all about the sauce!</p>
+    </div>
+  </div>
+  
+  <hr >
+
+  <div class="w3-row w3-padding-64" id="about">
+  <div class="w3-col m6 w3-padding-large w3-hide-small">
+  <img src="images/home6.jpg" class="w3-round w3-image w3-opacity-min" alt="Table Setting" style="width:800px; 
+  height:590px;">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+     <h1 class="w3-center" style="color:black;">Chicken Biryani</h1><br>
+      <p class="w3-large"> Biryani is an epitome of Mughlai cuisine and over the years there have been several variations of this one dish. Chicken Biryani is one of the most popular dishes that you can find across the world and it’s loaded with various spices. It’s an interesting combination of rice and meat, and if you put your heart into it, people will love it for sure.
+      One of the best things about preparing biryani is that it has several layers resulting into different textures, flavours and colours.</p>
+      <p class="w3-large w3-text-grey w3-hide-medium">Chicken Biryani is one dish that can be enjoyed on several occasions such as buffet, kitty party, potluck and even game night. So, what are you waiting for? Try this lip-smacking delicacy and enjoy with salan and your favourite raita.</p>
+    </div>
+  </div>
+  
+  <hr >
+
+  <div class="w3-row w3-padding-64" id="about">
+  <div class="w3-col m6 w3-padding-large w3-hide-small">
+  <img src="images/home7.jpeg" class="w3-round w3-image w3-opacity-min" alt="Table Setting" style="width:800px; 
+  height:550px;">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+     <h1 class="w3-center" style="color:black;">Italian Pasta</h1><br>
+      <p class="w3-large">No matter the season, a perfect plate of pasta is one of life's simple pleasures. One of our favorite things about pasta is that the shape and sauce pairing combinations seem endless, each one more delicious than the last. When it comes to pasta, Italy is unrivalled beyond doubt. As the world's biggest producers and consumers Italians are probably the only people happy to eat pasta more than once every day.</p>
+      <p class="w3-large w3-text-grey w3-hide-medium">A wonderfully tasty Italian pasta filled with various flavours and topped with delicious sauces, our pasta is an instant classic, try it today!</p>
+    </div>
+  </div>
+  
+  <hr >
+</div>
+
+</div>
+
+
+<div class="abc" style="background-color:pink;black:white;padding:20px;">
+Contact Us Here <br>
+Spice Food Restaurant<br>
+Address: TE RAPA ROAD BASE<br>
+Timmings: 11:00am - 12:00pm Everyday<br>
+Phone: 0226035534<br>
+Email: khoslaabhi3@gmail.com<br>
+</div>
+
+
+
+
+
+</body>
+</html>
